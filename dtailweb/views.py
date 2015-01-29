1f69749349bca74ee8d8e4d7cc4e58e284d77a0b
Alessandro <kappaciccio@gmail.com>
2015-01-29 15:46:55 +0100

    test page with material design css

---
+++ dtailweb/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect
+from django.template.loader import render_to_string
+from django.template import RequestContext
+
+
+def test(request):
+    return render_to_response('base.html', RequestContext(request, {}))
